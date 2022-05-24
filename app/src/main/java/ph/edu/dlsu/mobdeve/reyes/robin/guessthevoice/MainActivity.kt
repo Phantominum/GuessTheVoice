671b52d35f82a14be2d40544d88ee66ba3f9e94b
Phantominum <robinjerome23@gmail.com>
2022-05-25 00:58:55 +0800

    updated font files, and created quiz list card view

--- app/src/main/java/ph/edu/dlsu/mobdeve/reyes/robin/guessthevoice/MainActivity.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/reyes/robin/guessthevoice/MainActivity.kt
@@ -2,10 +2,23 @@ package ph.edu.dlsu.mobdeve.reyes.robin.guessthevoice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import ph.edu.dlsu.mobdeve.reyes.robin.guessthevoice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.logIn.setOnClickListener{
+            view: View?-> Toast.makeText(applicationContext, "Logged in", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
