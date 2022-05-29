2320c4e744b3932a398eb984968b31c6bf01870b
Kirsten S <daniellesison19@gmail.com>
2022-05-30 03:55:49 +0800

    added quiz maker fragments

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/reyes/robin/guessthevoice/fragments/QuizStepOne.kt
@@ -0,0 +1,7 @@
+package ph.edu.dlsu.mobdeve.reyes.robin.guessthevoice.fragments
+
+import androidx.fragment.app.Fragment
+import ph.edu.dlsu.mobdeve.reyes.robin.guessthevoice.R
+
+class QuizStepOne : Fragment(R.layout.quiz_step_one) {
+}
