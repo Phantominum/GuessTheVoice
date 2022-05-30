f0f82e06db7252f37cb10d35dea73a3e88fef719
Kirsten S <daniellesison19@gmail.com>
2022-05-31 02:34:24 +0800

    added kirsten changes

---
+++ app/src/main/java/ph/edu/dlsu/mobdeve/reyes/robin/guessthevoice/fragments/QuizStepTwo.kt
@@ -0,0 +1,7 @@
+package ph.edu.dlsu.mobdeve.reyes.robin.guessthevoice.fragments
+
+import androidx.fragment.app.Fragment
+import ph.edu.dlsu.mobdeve.reyes.robin.guessthevoice.R
+
+class QuizStepTwo: Fragment(R.layout.quiz_step_two) {
+}
