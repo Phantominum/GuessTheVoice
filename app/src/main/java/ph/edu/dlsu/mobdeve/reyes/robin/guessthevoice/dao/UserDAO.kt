27e6fb435cc91894e5552fca524cc98588541637
Phantominum <robinjerome23@gmail.com>
2022-06-26 01:34:33 +0800

    changed database creation of accounts from realtime to cloud firestore

--- app/src/main/java/ph/edu/dlsu/mobdeve/reyes/robin/guessthevoice/dao/UserDAO.kt
+++ app/src/main/java/ph/edu/dlsu/mobdeve/reyes/robin/guessthevoice/dao/UserDAO.kt
@@ -5,22 +5,26 @@ import android.widget.Toast
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import ph.edu.dlsu.mobdeve.reyes.robin.guessthevoice.model.User
 
 class UserDAO(var ctx: Context) {
     private val DB_URL:String = "https://guessthevoice-default-rtdb.asia-southeast1.firebasedatabase.app/"
-    private var database: FirebaseDatabase
-    private var dbRef: DatabaseReference
+    private var database: FirebaseFirestore
+//    private var dbRef: DatabaseReference
 
     init {
-        database = Firebase.database(DB_URL)
-        dbRef = database.getReference("Accounts")
+//        database = Firebase.database(DB_URL)
+        database = Firebase.firestore
+//        dbRef = database.getReference("Accounts")
+//        db.collection.
     }
 
     fun createAccount(user: User): Int {
         var success = 0
-        dbRef.child(user.username).setValue(user).addOnSuccessListener {
+        database.collection("Accounts").add(user).addOnSuccessListener {
             Toast.makeText(ctx, "Successfully registered!",Toast.LENGTH_SHORT).show()
             success = 1
         }.addOnFailureListener {
@@ -31,9 +35,10 @@ class UserDAO(var ctx: Context) {
 
     fun getAccount(username: String): User? {
         var user : User? = null
-        dbRef.child(username).get().addOnSuccessListener {
-            if (it.exists()) {
-                user = it.getValue(User::class.java)
+        database.collection("Accounts").whereEqualTo("username",username).get().addOnSuccessListener {
+            if (!it.isEmpty) {
+                user = it.documents[0].toObject(User::class.java)
+                println("LOG: ${user.toString()}")
                 if (user != null)
                     println("LOG: Received ${user!!.username} data")
             } else {
